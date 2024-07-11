480e2108e173ea43c41dd64379e15a6fced411d8
irina744 <bel.iri.s@mail.ru>
2024-07-11 20:33:34 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 'a', 'b', 'c', 'avto'
+print(immutable_var)
+mutable_list = [1, 2, 3, 'w', "avto"]
+print(mutable_list)
+mutable_list[1] = 5
+mutable_list.append('www')
+print(mutable_list)
