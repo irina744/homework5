d98bf6eea2f18485ca030d1276b6c1ba2c6d984d
irina744 <bel.iri.s@mail.ru>
2024-07-15 16:43:04 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,7 +1,10 @@
 immutable_var = 1, 2, 3, 'a', 'b', 'c', 'avto'
 print(immutable_var)
+#immutable_var_[0] = 5
+#print(immutable_var)
 mutable_list = [1, 2, 3, 'w', "avto"]
 print(mutable_list)
 mutable_list[1] = 5
 mutable_list.append('www')
 print(mutable_list)
+print('кортеж - это неизменяемая упорядоченная коллекция, используется в качестве хранилища(того,что мы не хотим трогать и менять) и занимает в памяти меньше места, чем список')
